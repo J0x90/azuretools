5d7df05a59ec0f6e8ddcc008460f7049a60d09bc
unknown <joshua.phillips@amtrak.com>
2023-04-27 17:56:41 -0400

    update

--- cefpy-original.sh
+++ cefpy-original.sh
@@ -20,7 +20,7 @@ sudo lvextend --size +28G /dev/mapper/rootvg-rootlv
 #sudo sed -i '65 s/^/#/' /etc/rsyslog.conf
 sudo firewall-cmd --permanent --zone=public --add-port=514/tcp
 sudo firewall-cmd --permanent --zone=public --add-port=514/udp
-sudo firewall-cmd –reload
+sudo firewall-cmd --reload
 wget -O Forwarder_AMA_installer.py https://raw.githubusercontent.com/schoonercg/azuretools/master/Forwarder_AMA_installer.py
 sudo python3 Forwarder_AMA_installer.py
 #sudo sleep 60s
